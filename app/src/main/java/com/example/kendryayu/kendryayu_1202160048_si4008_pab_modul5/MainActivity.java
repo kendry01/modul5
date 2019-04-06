577b43f3ca7cbfad9d21d4b3f8110855a93f7160
Kendry Ayu <kendryayu98@gmail.com>
2019-04-06 10:06:41 +0700

    push

---
+++ app/src/main/java/com/example/kendryayu/kendryayu_1202160048_si4008_pab_modul5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kendryayu.kendryayu_1202160048_si4008_pab_modul5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
